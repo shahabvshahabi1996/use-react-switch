e93a24e9cc7808bea434f8fee4d1197c2ec23195
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2019-04-26 23:46:07 +0330

    Added PropTypes and containerStyles and toggleStyles

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+import './styles.css';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export default class Switch extends Component {
+  
+  static PropTypes = {
+    isOpen : PropTypes.bool.isRequired,
+    onClick : PropTypes.func,
+    containerStyles : PropTypes.object,
+    toggleStyles : PropTypes.object,
+    name : PropTypes.string,
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      active : false
+    }
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      let {active} = this.state;
+      return (    
+          <div onClick={this.props.onClick} className="switch-wrapper">
+              <div style={this.props.containerStyles} className={`switch-body ${this.props.isOpen ? 'switch-active' : ''}`}>
+                  <div style={this.props.toggleStyles} className={`switch-toggle ${this.props.isOpen ? 'switch-toggle-active' : ''}`}></div>
+              </div>
+              <span>{this.props.placeholder}</span>
+              <input checked={active} name={this.props.name} type="checkbox" />
+          </div>
+      )
   }
 }
